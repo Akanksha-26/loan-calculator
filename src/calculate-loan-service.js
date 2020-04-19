6708fbd2ced0cde223aa60e488c297530f040b3c
Akanksha-26 <akanksha.s9560@gmail.com>
2020-04-19 20:44:16 +0200

    removed files

--- src/calculate-loan-service.js
+++ src/calculate-loan-service.js
@@ -1,7 +1,8 @@
 export class CalaculateLoanService {
     async calculate(amount, time) {
-        const response = await fetch("http://www.mocky.io/v2/5e9b6b623300006100bf1798", {
+        const response = await fetch("https://www.mocky.io/v2/5e9c972530000059000a7f0a", {
             method: 'POST',
+            headers: { 'Access-Control-Allow-Origin': '*' },
             body: {
                 amount: amount,
                 duration: time
