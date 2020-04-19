df884cfc897f12758b7d3e725df4d3c362938513
Akanksha-26 <akanksha.s9560@gmail.com>
2020-04-19 20:55:27 +0200

    added build

---
+++ build/precache-manifest.6da443b8f4a71da1cbeb2ce9ee12a776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63739cbdb83d57323a66300042659f9f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a2455c9ca1621bce1691",
+    "url": "./static/css/main.7d47fb89.chunk.css"
+  },
+  {
+    "revision": "2ba2ea853fce6ce2b22c",
+    "url": "./static/js/2.07942205.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.07942205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2455c9ca1621bce1691",
+    "url": "./static/js/main.051d303e.chunk.js"
+  },
+  {
+    "revision": "47db332d88e9c320494d",
+    "url": "./static/js/runtime-main.283f2da0.js"
+  }
+]);
